145a82dc4c173cdbc39e2094bdb6bfd01dfef4d3
Chloe Wong <70902973+chloehkwong1@users.noreply.github.com>
2023-04-28 17:38:06 +0100

    Move image to assets

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,5 @@ void main() {
       home: Scaffold(
           backgroundColor: Colors.pink[100],
           appBar: AppBar(title: Text('I Am Rich'), backgroundColor: Colors.pink[300]),
-          body: Center(
-              child: Image.network(
-                  'https://www.sixt.co.uk/magazine/wp-content/uploads//sites/11/2023/01/valencia-main-1024x642.jpg')))));
+          body: Center(child: Image(image: AssetImage('images/valencia.jpg'))))));
 }
