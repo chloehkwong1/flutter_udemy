541e7d591ecbf18c710e1747f513c2cef29d892b
Chloe Wong <70902973+chloehkwong1@users.noreply.github.com>
2023-04-28 17:25:28 +0100

    Add app bar, colour and a picture to home page

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,10 @@ import 'package:flutter/material.dart';
 // The main function is the starting point for all our Flutter apps
 void main() {
   runApp(MaterialApp(
-    home: Center(
-      child: Text('Hello World'),
-    ),
-  ));
+      home: Scaffold(
+          backgroundColor: Colors.pink[100],
+          appBar: AppBar(title: Text('I Am Rich'), backgroundColor: Colors.pink[300]),
+          body: Center(
+              child: Image.network(
+                  'https://www.sixt.co.uk/magazine/wp-content/uploads//sites/11/2023/01/valencia-main-1024x642.jpg')))));
 }
